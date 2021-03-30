01ae3ca21cb7013df9b8290ff7f05d5e9dfaae32
michalinh0 <machowski.michal01@gmail.com>
2021-03-30 23:38:54 +0200

    Created function calculating consistent savings without belko tax

--- financialcalculator.py
+++ financialcalculator.py
@@ -3,9 +3,21 @@ class FinancialCalculator:
         pass
 
     @staticmethod
-    def calculate_consistent_savings():
-        pass
-    
+    def calculate_consistent_savings_no_belko_tax(deposit, years, rate):
+        sum = 0
+        for x in range (0, years):
+            sum += deposit
+            sum *= (1+rate/12)
+        return sum
+
 
 if __name__ == "__main__":
-    print("It works!")
+    deposit=input("Tell me your monthly deposit : ")
+    years=input("Tell me how long you want to save in years : ")
+    rate=input("Tell me the rate of return ( 1% = 0.01 ) : ")
+    deposit = int(deposit)
+    years=int(years)*12
+    rate=float(rate)
+    result =FinancialCalculator.calculate_consistent_savings_no_belko_tax(deposit, years, rate)*100
+    result =(round(result))/100
+    print("After " + str(years/12) + " years you will have "+ str(result) + " zl ( excluding Belko's tax )")
