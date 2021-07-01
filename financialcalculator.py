9ba6e01acf737ff9b8268fa7b1fe788ec154b1d2
cieslakamil <kamilciesla34@gmail.com>
2021-07-01 18:20:50 +0200

    hotfixes

--- financialcalculator.py
+++ financialcalculator.py
@@ -13,12 +13,12 @@ class FinancialCalculator:
 
     @staticmethod
     def calculate_savings(deposit, months, rate, include_beam_tax):
-        """Calculate amount of money you will have after specified duration and monthly investment"""
+        """Calculate how much you save after given time in months, annual interest rate"""
         sum = 0
         monthly = [0]
         for x in range(months):
             sum += deposit
-            interest = sum*(rate/12)
+            interest = sum*(rate*0.01/12)
             #interest = (round(interest*100))/100
             if include_beam_tax:
                 interest = FinancialCalculator.apply_beam_tax(interest)
@@ -28,22 +28,19 @@ class FinancialCalculator:
         return monthly
 
     @staticmethod
-    def calculate_investement(money, duration, rate):
-        sum = money
-        for x in range(duration):
-            interest = sum*(rate/12*0.01)
-            sum += interest
-        sum -=money
+    def calculate_investement(money, months, rate):
+        """Calculate the profit of investement after given months and rate."""
+        interest = money * (months*rate/12*0.01)
         return {
-            "interest": sum,
-            "interest_with_beam_tax": FinancialCalculator.apply_beam_tax(sum)
+            "interest": interest,
+            "interest_with_beam_tax": FinancialCalculator.apply_beam_tax(interest)
         }
 
     @staticmethod
     def calculate_interest_saving_time(expected, initial, savings, rate):
         """Calculate the number of months needed to save given amount of money."""
         saving_months = 0
-        sum = initial
+        sum = initials
         if initial >= expected:
             not_found = False
         else:
@@ -64,9 +61,97 @@ class FinancialCalculator:
 
     @staticmethod
     def calculate_installment(loan, duration, rate):
+        """Calculate an installment for a given loan."""
         sum = 0
         factor = 1+(rate/12)
         for x in range(1, duration+1):
             sum += factor**-x
         installment = loan/sum
         return installment
+
+
+if __name__ == "__main__":
+    deposit = input("Tell me your monthly deposit : ")
+    years = input("Tell me how long you want to save in years : ")
+    rate = input("Tell me the rate of return ( 1% = 0.01 ) : ")
+    deposit = int(deposit)
+    years = int(years)*12
+    rate = float(rate)
+    beam_tax = False
+    result = FinancialCalculator.calculate_savings(
+        deposit, years, rate, beam_tax)
+    for x in range(years):
+        result[x-1] = (round(result[x-1]*100))/100
+    print("After " + str(years/12) + " years you will have " +
+          str(result[years]) + " zl ( excluding beam_tax's tax )")
+    beam_tax = True
+    result = FinancialCalculator.calculate_savings(
+        deposit, months, rate, beam_tax)
+    for value in result:
+        rounded_result.append(round(value, 2))
+    print(rounded_result)
+    #print("After " + str(months/12) + " years you will have " +      str(result[months]) + " zl ( including beam_tax's tax )")
+    #print(f'After {months/12} years you will have {rounded_result[months]} zł (including beam tax)')
+
+    money = input("Tell me how much money you can deposit : ")
+    duration = input("Tell me how long the deposit will be ( in months ): ")
+    returnrate = input("Tell me the yearly rate of return ( 1% = 0.01 ): ")
+    money = int(money)
+    duration = int(duration)
+    returnrate = float(returnrate)
+    interest = round(FinancialCalculator.calculate_investement(
+        money, duration, returnrate), 2)
+    print(f'Interest after substracting beam tax will be {interest} zl')
+
+    expected = input("Tell me how much you want to have : ")
+    initial = input("Tell me how much you can invest initially : ")
+    savings = input("Tell me how much you can save every month : ")
+    rate_of_return = input("Tell me yearly rate of return ( 1% = 0.01 ) : ")
+    expected = int(expected)
+    initial = int(initial)
+    savings = int(savings)
+    rate_of_return = float(rate_of_return)
+    saving_duration = FinancialCalculator.calculate_interest(
+        expected, initial, savings, rate_of_return)
+    if saving_duration == 0:
+        print("You already have expected amount of money")
+    else:
+        print(f'You will need to save for {saving_duration} months')
+    loan = input("Tell me how much you want to loan : ")
+    duration = input("Tell me how long ( in months ) loan will be : ")
+    rate = input("Tell me rate of loan ( 1% = 0.01 ) : ")
+    loan = int(loan)
+    duration = int(duration)
+    rate = float(rate)
+    installment = FinancialCalculator.calculate_installment(
+        loan, duration, rate)
+    installment = round(installment, 2)
+    print(f'Monthly installment will be {installment} zl')
+    '''
+    amount = input("Tell me how many loans you want to connect : ")
+    amount  = int(amount)
+    suma = 0
+    kredyt = 0 
+    for x in range (amount):
+        loan = input("Tell me how much you want to loan : ")
+        duration = input("Tell me how long ( in months ) loan will be : ")
+        rate = input("Tell me rate of loan ( 1% = 0.01 ) : ")
+        loan = int(loan)
+        kredyt += loan
+        duration = int(duration)
+        rate = float(rate)
+        suma += FinancialCalculator.calculate_installment(loan , duration, rate)
+        suma = round(suma,2)
+    duration = input("Tell me how long ( in months ) consolidation loan will be : ")
+    rate = input("Tell me rate of consolidation loan ( 1% = 0.01 ) : ")
+    duration = int(duration)
+    rate = float(rate)
+    #suma = suma/duration
+    #suma = round(suma,2)
+    result = FinancialCalculator.calculate_installment(kredyt, duration, rate)
+    result = round(result,2)
+    print (f'Monthly installment of consolidation loan will be {result} zl')
+    suma -= result
+    suma = round(suma,2)
+    print (f'Monthly installment will be lower by {suma} zl')
+    '''
